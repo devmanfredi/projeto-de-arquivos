10d3f6f1c0a5160b5f11f5e3e9aff2117c12a77a
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-03-13 21:10:04 -0300

    Fist commit

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+    int tamanho;
+    int i, j = 0;
+    int soma, valor = 0;
+    int *p;
+
+    printf("Digite o tamanho do vetor: \n");
+    scanf("%d", &tamanho);
+
+    printf("Digite o valor da soma: \n");
+    scanf("%d", &valor);
+
+    p = malloc(tamanho * sizeof(int));
+
+
+    for(i = 0; i < tamanho; i++){
+        printf("Digite os elementos do vetor V[%d]: \n", i);
+        scanf("%d", &p[i]);
+    }
+
+    for(i = 0; i < tamanho; i++){
+        for(j = i + 1; j < tamanho; j++){
+            soma = p[i] + p[j];
+            if(soma == valor) {
+                printf("\t**********TRUE*********\t");
+            return 1;
+            }
+        }
+
+    }
+
+
+}
+
